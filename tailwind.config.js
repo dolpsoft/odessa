66d4a6250b4d41992c17e2b1549033932502cede
dolpsoft <rekld432@gmail.com>
2023-03-10 21:22:26 +0300

    Frontend güncellemesi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,css,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "linear-gradient(to bottom, #00000080, #00000080), url('../media/images/v.jpg')",
+        'hero-pattern-2': "linear-gradient(to bottom, #00000080, #00000080), url('../media/images/anime-pic.jpg')",
+     },
+    },
+    fontFamily: {
+      sans : ["Poppins", 'sans-serif']
+    }
   },
   plugins: [],
 }
+// url(./src/anime-pic.jpg
